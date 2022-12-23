812cc14e8925bc6f9569d0db499301e15dbd14d3
Oppo691 <ifeanyisaintemmanuel@gmail.com>
2022-12-23 15:50:45 -0800

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 -encodes using rot13
+ * @s:pointer to string parameters
+ * Return: encoded strings
+ */
+char *rot13(char *s)
+{
+	int a;
+	int b;
+
+	char alphabet{} = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char rot13key[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b< 52; b++)
+		{
+			if (s[a] == alphabet[b])
+			{
+				s[a] = rot13key[b];
+				break;
+			}
+		}
+	}
+	return (s);
+}
