2cec669d593e559b35587eed3101939cd879b53f
oppo691 <ifeanyisaintemmanuel@gmail.com>
2023-01-21 15:49:25 -0800

    task 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings.
+ * @seperator: The string to display betwwen two strings.
+ * @n: The number of string to print
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
+{
